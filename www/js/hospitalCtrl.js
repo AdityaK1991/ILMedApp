bad0e8da3fe193a65bdde7665440d3d64e90b70b
AdityaK1991 <kulkarni.aditya.p@gmail.com>
2015-10-21 21:05:29 -0500

    hospital merge

--- www/js/hospitalCtrl.js
+++ www/js/hospitalCtrl.js
@@ -1,29 +1,46 @@
 angular.module('starter.controllers')
 
-.controller('HospitalCtrl', function($scope, HospitalData) {
+.controller('HospitalCtrl', function($scope, HospitalData, $ionicLoading) {
 
   $scope.$on('$ionicView.beforeEnter', function(e) {
     
+        $ionicLoading.show({
+          content: 'Loading...',
+          animation: 'fade-in',
+          showBackdrop: true,
+          maxWidth: 200,
+          showDelay: 0
+        });
+    
+   
+
      $scope.hospital={}
 
-     $scope.hospitals = HospitalData.AllResponses.query();
-     $scope.navigate=function(msg){
-      alert(msg);
-$location.path("/maps")
-     }
-
+     
      
   })
 
+  $scope.$on('$ionicView.afterEnter', function() {
 
-  $scope.searchHospitals = function(){
+     
+     $scope.hospitals = HospitalData.AllResponses.query();
+     $ionicLoading.hide();
+     $scope.navigate=function(msg){
+      alert(msg);
+     $location.path("/maps")
+     }
 
-	   $scope.hospitals = HospitalData.Responses.query({ type_of_hospital:$scope.hospital.choice, city:$scope.hospital.city });  
+  })
 
-		   //console.log($scope.hello)
-		   //console.log($scope.hosp)
+
+  // $scope.searchHospitals = function(){
+
+	 //   $scope.hospitals = HospitalData.Responses.query({ type_of_hospital:$scope.hospital.choice, city:$scope.hospital.city });  
+
+		//    //console.log($scope.hello)
+		//    //console.log($scope.hosp)
    
-      } 
+  //     } 
 
    $scope.toggleDetails = function(q){
    		$scope[q] = !$scope[q]
